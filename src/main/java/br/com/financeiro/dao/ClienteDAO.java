02700333d1ab1a4776e7ab2cd95c572a0eab63ba
pauloricardolongi <pauloricardolongi@gmail.com>
2022-07-14 18:27:47 -0300

    segundo

---
+++ src/main/java/br/com/financeiro/dao/ClienteDAO.java
@@ -0,0 +1,8 @@
+package br.com.financeiro.dao;
+
+import br.com.financeiro.domain.Cliente;
+
+
+public class ClienteDAO extends GenericDAO<Cliente>{
+
+}
---
+++ src/main/java/br/com/financeiro/dao/ProdutoDAO.java
@@ -0,0 +1,7 @@
+package br.com.financeiro.dao;
+
+import br.com.financeiro.domain.Produto;
+
+public class ProdutoDAO extends GenericDAO<Produto>{
+
+}
